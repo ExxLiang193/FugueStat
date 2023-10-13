Fixed small order of calculation error

algorithm/adaptive_edit_distance.py
@@ -18,13 +18,13 @@ class AdaptiveEditDistance:
         memo: np.array = np.zeros((S + 1, P + 1))
         for i in range(1, S + 1):
             for j in range(1, P + 1):
-                memo[i][j] = min(self.scale(metric(memo, self.stream, self.pattern, i, j)) for metric in self.metrics)
+                memo[i][j] = min(metric(memo, self.stream, self.pattern, i, j, self.scale) for metric in self.metrics)
         return memo
 
     def get_rank(self, min_match: int) -> List[RankInfo]:
         S, P = len(self.stream), len(self.pattern)
         diagonal_offset: int = S - P
-        limits: Tuple[int, int] = (min(0, -P + min_match), max(2, S - min_match + 1))
+        limits: Tuple[int, int] = (min(-1, -P + min_match), max(2, S - min_match + 1))
 
         def normalized_diagonal_distance(memo: np.array, offset: int) -> float:
             offset_diag = np.diagonal(memo, offset=offset)

algorithm/model/distance_metrics.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Iterable
+from typing import TYPE_CHECKING, Iterable, Callable
 from functools import lru_cache
 import math
 import numpy as np
@@ -7,45 +7,55 @@ import numpy as np
 class ScalingFunctions:
     @classmethod
     @lru_cache(maxsize=32)
-    def sqrt(cls, value: int, scale: int = 10) -> float:
+    def sqrt(cls, value: int, scale: int = 2) -> float:
         return math.sqrt(scale * value)
 
     @classmethod
     @lru_cache(maxsize=32)
-    def floored_sqrt(cls, value: int, scale: int = 10) -> float:
+    def floored_sqrt(cls, value: int, scale: int = 2) -> float:
         return math.floor(math.sqrt(scale * value))
 
 
 class DistanceMetrics:
     @classmethod
-    def replacement(cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int) -> int:
-        return memo[cur_i - 1][cur_j - 1] + abs(x[cur_i - 1] - y[cur_j - 1])
+    def replacement(cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int, scale: Callable) -> int:
+        return memo[cur_i - 1][cur_j - 1] + scale(abs(x[cur_i - 1] - y[cur_j - 1]))
 
     @classmethod
-    def replacement_with_penalty(cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int) -> int:
+    def replacement_with_penalty(
+        cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int, scale: Callable
+    ) -> int:
         penalty = 1 if (x[cur_i - 1] < 0) == (y[cur_j - 1] < 0) else 2
-        return memo[cur_i - 1][cur_j - 1] + penalty * abs(x[cur_i - 1] - y[cur_j - 1])
+        return memo[cur_i - 1][cur_j - 1] + scale(penalty * abs(x[cur_i - 1] - y[cur_j - 1]))
 
     @classmethod
-    def insertion_without_expansion(cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int) -> int:
-        return memo[cur_i][cur_j - 1] + abs(y[cur_j - 1])
+    def insertion_without_expansion(
+        cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int, scale: Callable
+    ) -> int:
+        return memo[cur_i][cur_j - 1] + scale(abs(y[cur_j - 1]))
 
     @classmethod
-    def insertion_with_expansion(cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int) -> int:
+    def insertion_with_expansion(
+        cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int, scale: Callable
+    ) -> int:
         return (
             float("inf")
             if cur_j < 2
-            else memo[cur_i - 1][cur_j - 2] + abs((y[cur_j - 2] + y[cur_j - 1]) - x[cur_i - 1])
+            else memo[cur_i - 1][cur_j - 2] + scale(abs((y[cur_j - 2] + y[cur_j - 1]) - x[cur_i - 1]))
         )
 
     @classmethod
-    def deletion_without_compression(cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int) -> int:
-        return memo[cur_i - 1][cur_j] + abs(x[cur_i - 1])
+    def deletion_without_compression(
+        cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int, scale: Callable
+    ) -> int:
+        return memo[cur_i - 1][cur_j] + scale(abs(x[cur_i - 1]))
 
     @classmethod
-    def deletion_with_compression(cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int) -> int:
+    def deletion_with_compression(
+        cls, memo: np.array, x: Iterable, y: Iterable, cur_i: int, cur_j: int, scale: Callable
+    ) -> int:
         return (
             float("inf")
             if cur_i < 2
-            else memo[cur_i - 2][cur_j - 1] + abs((x[cur_i - 2] + x[cur_i - 1]) - y[cur_j - 1])
+            else memo[cur_i - 2][cur_j - 1] + scale(abs((x[cur_i - 2] + x[cur_i - 1]) - y[cur_j - 1]))
         )